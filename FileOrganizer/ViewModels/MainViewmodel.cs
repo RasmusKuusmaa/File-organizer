bcb2dcd04ef022e0f2d66180505741ea4380adab
77f315 <77f315@gmail.com>
2025-11-13 10:36:44 +0200

    select folder

---
+++ FileOrganizer/ViewModels/MainViewmodel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Forms;
+using FileOrganizer.helpers;
+namespace FileOrganizer.ViewModels
+{
+    public class MainViewmodel : BaseViewModel
+    {
+        private string _folderPath;
+
+        public string FolderPath
+        {
+            get { return _folderPath; }
+            set { _folderPath = value;
+                OnProperttychanged();
+            }
+        }
+        public ICommand SelectFolderCommand { get; }
+        private void SelectFolder()
+        {
+            var dialog = new FolderBrowserDialog();
+            var result = dialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                FolderPath = dialog.SelectedPath;
+            }
+
+        }
+        public MainViewmodel()
+        {
+            SelectFolderCommand = new RelayCommand(o => SelectFolder());
+        }
+    }
+}
