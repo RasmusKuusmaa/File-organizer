6e80ad71924d910565844ad89358f5b0f9c7ba05
77f315 <77f315@gmail.com>
2025-11-13 10:52:26 +0200

    organize files

--- FileOrganizer/ViewModels/MainViewmodel.cs
+++ FileOrganizer/ViewModels/MainViewmodel.cs
@@ -28,10 +28,35 @@ namespace FileOrganizer.ViewModels
         public ObservableCollection<FileItem> Files { get; set; } = new();
 
         public ICommand SelectFolderCommand { get; }
+        public ICommand MoveFilesCommand { get; }
 
         public MainViewModel()
         {
             SelectFolderCommand = new RelayCommand(o => SelectFolder());
+            MoveFilesCommand = new RelayCommand(o => MoveFiles(), o => Files.Count > 0);
+
+        }
+        private void MoveFiles()
+        {
+            if (!Directory.Exists(FolderPath)) return;
+
+            foreach (var fileItem in Files.ToList()) 
+            {
+                var extension = Path.GetExtension(fileItem.FilePath).TrimStart('.').ToUpper();
+                var targetDir = Path.Combine(FolderPath, extension);
+
+                if (!Directory.Exists(targetDir))
+                    Directory.CreateDirectory(targetDir);
+
+                var targetPath = Path.Combine(targetDir, Path.GetFileName(fileItem.FilePath));
+
+                if (!File.Exists(targetPath))
+                {
+                    File.Move(fileItem.FilePath, targetPath);
+                }
+            }
+
+            LoadFilesByType();
         }
 
         private void SelectFolder()
