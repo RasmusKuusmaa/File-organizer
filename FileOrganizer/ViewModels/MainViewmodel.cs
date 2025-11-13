281041abcf1c24991c168b1617fd17d7eae098d4
77f315 <77f315@gmail.com>
2025-11-13 10:47:27 +0200

    display file icons

--- FileOrganizer/ViewModels/MainViewmodel.cs
+++ FileOrganizer/ViewModels/MainViewmodel.cs
@@ -1,46 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Windows.Input;
 using System.Windows.Forms;
 using FileOrganizer.helpers;
-using System.Collections.ObjectModel;
-using System.IO;
+using FileOrganizer.Models;
+using FileOrganizer.helpers;
+
 namespace FileOrganizer.ViewModels
 {
-    public class MainViewmodel : BaseViewModel
+    public class MainViewModel : BaseViewModel
     {
         private string _folderPath;
-
         public string FolderPath
         {
-            get { return _folderPath; }
-            set { _folderPath = value;
-                OnProperttychanged();
-            }
+            get => _folderPath;
+            set { _folderPath = value; OnProperttychanged(); }
         }
-        public ObservableCollection<string> Files { get; set; } = new ObservableCollection<string>();
+
+        private string _fileType;
+        public string FileType
+        {
+            get => _fileType;
+            set { _fileType = value; OnProperttychanged(); LoadFilesByType(); }
+        }
+
+        public ObservableCollection<FileItem> Files { get; set; } = new();
+
         public ICommand SelectFolderCommand { get; }
+
+        public MainViewModel()
+        {
+            SelectFolderCommand = new RelayCommand(o => SelectFolder());
+        }
+
         private void SelectFolder()
         {
             var dialog = new FolderBrowserDialog();
-            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            if (dialog.ShowDialog() == DialogResult.OK)
             {
                 FolderPath = dialog.SelectedPath;
-
-                Files.Clear();
-                foreach (var file in Directory.GetFiles(FolderPath))
-                {
-                    Files.Add(Path.GetFileName(file));
-                }
+                LoadFilesByType();
             }
         }
 
-        public MainViewmodel()
+        private void LoadFilesByType()
         {
-            SelectFolderCommand = new RelayCommand(o => SelectFolder());
+            if (!Directory.Exists(FolderPath)) return;
+
+            Files.Clear();
+            foreach (var file in Directory.GetFiles(FolderPath))
+            {
+                if (string.IsNullOrEmpty(FileType) || Path.GetExtension(file).Equals(FileType, StringComparison.OrdinalIgnoreCase))
+                {
+                    Files.Add(new FileItem(file));
+                }
+            }
         }
     }
 }
